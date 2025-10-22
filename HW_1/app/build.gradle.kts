18f99c83d23476cf97e5f6381fa721265f170f7c
jakelord1 <jakelord1.vo@gmail.com>
2025-10-22 23:03:50 +0300

    HW2

--- HW_1/app/build.gradle.kts
+++ HW_1/app/build.gradle.kts
@@ -14,8 +14,6 @@ android {
         targetSdk = 36
         versionCode = 1
         versionName = "1.0"
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
     buildTypes {
@@ -38,7 +36,4 @@ dependencies {
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.ext.junit)
-    androidTestImplementation(libs.espresso.core)
 }
